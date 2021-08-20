b8549d31748252e45ae8ff7dad8a21b941d1d5f6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-08-20 17:12:26 +0200

    Add default template MVC app for reference

---
+++ template/THNETII.WebServices.MvcTemplateWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace THNETII.WebServices.MvcTemplateWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; init; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ template/THNETII.WebServices.MvcTemplateWebApp/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace THNETII.WebServices.MvcTemplateWebApp
+{
+    public class Program
+    {
+        public static Task<int> Main(string[]? args)
+        {
+            RootCommand rootCommand = new()
+            {
+                Handler = CommandHandler.Create(
+                    (IHost host, CancellationToken cancelToken) =>
+                        host.RunAsync(cancelToken)
+                )
+            };
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseDefaults()
+                .UseHost(CreateHostBuilder)
+                .Build();
+            return parser.InvokeAsync(args ?? Array.Empty<string>());
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[]? args) =>
+            Host.CreateDefaultBuilder(args ?? Array.Empty<string>())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
