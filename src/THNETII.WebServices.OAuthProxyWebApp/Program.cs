8c79710059048129c20b9dad71068cb76ec70258
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-08-20 17:08:00 +0200

    Add System.CommandLine.Hosting

--- src/THNETII.WebServices.OAuthProxyWebApp/Program.cs
+++ src/THNETII.WebServices.OAuthProxyWebApp/Program.cs
@@ -1,4 +1,11 @@
 using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -7,9 +14,20 @@ namespace THNETII.WebServices.OAuthProxyWebApp
 {
     public static class Program
     {
-        public static void Main(string[]? args)
+        public static Task<int> Main(string[]? args)
         {
-            CreateHostBuilder(args).Build().Run();
+            RootCommand rootCommand = new()
+            {
+                Handler = CommandHandler.Create(
+                    (IHost host, CancellationToken cancelToken) =>
+                        host.RunAsync(cancelToken)
+                )
+            };
+            var parser = new CommandLineBuilder(rootCommand)
+                .UseDefaults()
+                .UseHost(CreateHostBuilder)
+                .Build();
+            return parser.InvokeAsync(args ?? Array.Empty<string>());
         }
 
         public static IHostBuilder CreateHostBuilder(string[]? args) =>
